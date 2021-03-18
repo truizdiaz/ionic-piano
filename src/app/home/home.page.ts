93dd0c92f51d2aa10376c3efd1a3ca64292c4707
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-18 16:57:52 -0300

    Finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  listElements: any[] = [
+    { numeroSonido: 1, color: '#0d6efd' },
+    { numeroSonido: 2, color: '#6c757d' },
+    { numeroSonido: 3, color: '#198754' },
+    { numeroSonido: 4, color: '#dc3545' },
+    { numeroSonido: 5, color: '#ffc107' },
+    { numeroSonido: 6, color: '#0dcaf0' },
+    { numeroSonido: 7, color: '#212529' },
+  ];
+
   constructor() {}
+
+  aplicarSonido(numeroSonido: number) {
+    const audio = new Audio();
+
+    audio.src = '../../assets/sonidos/note'+numeroSonido+'.wav';
+    audio.load();
+    audio.play();
+  }
 
 }
